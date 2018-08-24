1e5e938a4942a56aa63d8a9b6d7202e96642968a
Abhinav Anand <abhinav_anand@live.com>
2018-08-24 14:52:32 +0530

    Wire up things with main file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include "list.h"
+#include "cursor_control.h"
+#include "normal_mode.h"
 
 using namespace std;
 
 
 int main() {
+    clear_screen();
+
     cout << "--------------BEGIN-----------" << endl;
     ls(".");
     cout << "--------------END-----------" << endl;
+
+    move_cursor_xy(0, 0);
+
+    switch_to_normal_mode();
+
     return 0;
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC = g++
 CFLAGS = -Wall -std=c++1y
-DEPS = util.h list.h
-OBJ = util.o list.o main.o
+DEPS = util.h list.h cursor_control.h normal_mode.h keymap.h
+OBJ = util.o list.o normal_mode.o cursor_control.o main.o
 
 %.o: %.cpp $(DEPS)
 	$(CC) $(CFLAGS) -c -o $@ $<
