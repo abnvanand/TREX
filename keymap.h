91b3b1106fb27bca12822aa4115a3ac40aa23cdd
Abhinav Anand <abhinav_anand@live.com>
2018-08-24 14:43:54 +0530

    Add keymaps ANSI Escape codes

---
+++ keymap.h
@@ -0,0 +1,15 @@
+#ifndef KEYMAP_H
+#define KEYMAP_H
+
+#define ARROW_UP 'A'
+#define ARROW_DOWN 'B'
+#define ARROW_RIGHT 'C'
+#define ARROW_LEFT 'D'
+
+#define COLON ':'
+#define QUIT 'q'
+
+#define ESCAPE 27
+#define ETX 3
+
+#endif //KEYMAP_H
