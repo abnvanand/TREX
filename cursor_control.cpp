aee4bd8b09faafb14aa53a223da38693a1a25268
Abhinav Anand <abhinav_anand@live.com>
2018-08-25 15:41:06 +0530

    Add cursor and window related helper methods

--- cursor_control.cpp
+++ cursor_control.cpp
@@ -33,3 +33,58 @@ void move_right(int steps) {
 void move_left(int steps) {
     cout << "\033[" << steps << "D";
 }
+
+void move_cursor(char direction) {
+    cout << "\033[" << 1 << direction;
+}
+
+void save_cursor_pos() {
+    cout << "\033[s";
+}
+
+void restore_cursor_pos() {
+    cout << "\033[u";
+}
+
+void clear_row(int row_number) {
+    save_cursor_pos();
+    move_cursor_xy(row_number, 1);
+    cout << "\033[K";
+    restore_cursor_pos();
+}
+
+void get_window_size(int &rows, int &cols) {
+    struct winsize ws;
+    ioctl(fileno(stdout), TIOCGWINSZ, &ws);
+    cols = ws.ws_col;
+    rows = ws.ws_row;
+}
+
+
+void draw_command_line() {
+    // get window size
+    int rows, cols;
+    get_window_size(rows, cols);
+
+    clear_row(rows);
+    move_cursor_xy(rows, 1);
+
+    cout << ":";
+}
+
+void draw_info_line(string current_mode) {
+    save_cursor_pos();
+    int rows, cols;
+    get_window_size(rows, cols);
+    clear_row(rows - 1);
+    move_cursor_xy(rows - 1, 1);
+    cout << current_mode;
+    restore_cursor_pos();
+}
+
+void print_coloured_output(string text) {
+    cout << "\033[1;31m" << text << "\033[0m";
+    int size = (int) text.size() + 2;
+    move_left(size);
+}
+
--- cursor_control.h
+++ cursor_control.h
@@ -2,6 +2,9 @@
 #define CURSOR_CONTROL_H
 
 #include <iostream>
+#include <sys/ioctl.h>
+
+#include "keymap.h"
 
 void clear_screen();
 
@@ -14,5 +17,25 @@ void move_down(int steps);
 void move_left(int steps);
 
 void move_right(int steps);
+
+/**
+ * Moves cursor 1 step in supplied direction
+ * @param direction
+ */
+void move_cursor(char direction);
+
+void save_cursor_pos();
+
+void restore_cursor_pos();
+
+void clear_row(int row_number);
+
+void draw_command_line();
+
+void draw_info_line(std::string current_mode);
+
+void print_coloured_output(std::string text);
+
+void execute_command(std::string &command);
 
 #endif //CURSOR_CONTROL_H
