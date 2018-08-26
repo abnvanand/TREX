e668d45f03645b3ca7ff8048c57d0140c9e556f6
Abhinav Anand <abhinav_anand@live.com>
2018-08-27 00:40:40 +0530

    Rewrite normal_mode :refactor:

--- normal_mode.cpp
+++ normal_mode.cpp
@@ -1,34 +1,19 @@
 #include "normal_mode.h"
-#include "util.h"
-#include "list.h"
 
 using namespace std;
 
 // We are in Normal mode (where you can't type anything exceptions are always there :)
 /**
- * receives initial termios settings and modifies its copy
  * it's the responsibility of this module to leave the terminal in the state it was
  * @param initial_settings
  * @return
  */
 int normal_mode() {
-//    clear_screen();
-
-    draw_info_line("NORMAL_MODE");
-    move_cursor_xy(1, 1);
-
-    cout << "-------------BEGIN-----------" << endl;
-    ls(".");
-    cout << "--------------END-----------" << endl;
-
-    move_cursor_xy(1, 1);
-
     struct termios initial_settings, new_settings;
     tcgetattr(fileno(stdin), &initial_settings);
     new_settings = initial_settings;
 
-    char ch;
-    int cause_of_return = RES_QUIT;
+    int cause_of_return;
 
     new_settings.c_lflag &= ~ICANON;
     new_settings.c_lflag &= ~ECHO;         // to hide typed entry
@@ -43,38 +28,128 @@ int normal_mode() {
         return RES_ERROR;
     }
 
-    do {
-        cin >> ch;
+    string dir = ".";
+    vector<string> back_stack;
+    vector<string> forward_stack;
 
-        switch (ch) {
-            case ARROW_UP:
-            case ARROW_DOWN:
-//            case ARROW_RIGHT:
-//            case ARROW_LEFT:
-                move_cursor(ch);
-                break;
+    back_stack.emplace_back(".");
 
-            case ETX:  // ETX (end of text) (Ctrl+C )
-                cout << "\nType q to quit\n";
-                break;
-            case QUIT:
-                // DO NOT return here. Need to restore the termios initial settings
-                cause_of_return = RES_QUIT;
-                break;
+    while (true) {
+        cause_of_return = refresh_normal_mode(dir, back_stack, forward_stack);
 
-            case COLON:
-                // FIXME: CAN NOT and MUST NOT Return here before restoring terminal state
-                cause_of_return = RES_COMMAND_MODE;
-                break;
-
-            default:
-                // cout << "\n You entered: " << ch << '\n';
-                break;
-        }
-
-    } while (ch != 'q' && ch != ':');
+        if (cause_of_return != RES_GOTO_DIR)
+            break;
+    }
 
     // MUST restore the terminal to the way it was
     tcsetattr(fileno(stdin), TCSANOW, &initial_settings);
     return cause_of_return;
+}
+
+int refresh_normal_mode(string &dir_name, vector<string> &back_stack, vector<string> &forward_stack) {
+    clear_screen();
+    string path;
+    join(back_stack, '/', path);
+    string info_line = "NORMAL_MODE: " + path;
+
+    draw_info_line(info_line);
+    move_cursor_xy(1, 1);
+
+    save_cursor_pos();  // save current cursor position it will to first item in list
+
+    vector<string> directory_list = ls(std::move(dir_name));
+
+    for (auto i:directory_list)
+        print_details(i);
+
+    move_cursor_xy(1, 1);
+
+    unsigned long current_index = 0;
+    unsigned long last_index = directory_list.size() - 1;
+
+    char ch;
+
+    while (true) {
+        ch = (char) cin.get();
+
+        if (ch == ARROW_UP) {
+            if (current_index > 0) {
+                scroll_up();
+                current_index -= 1;
+            }
+        } else if (ch == ARROW_DOWN) {
+            if (current_index < last_index) {
+                scroll_down();
+                current_index += 1;
+            }
+        } else if (ch == ARROW_LEFT) {
+            if (back_stack.size() == 1)
+                continue;    // do nothing
+
+            forward_stack.push_back(back_stack[back_stack.size() - 1]);
+            back_stack.pop_back();
+            dir_name = "..";
+            return RES_GOTO_DIR;
+
+        } else if (ch == ARROW_RIGHT) {
+            if (forward_stack.empty())
+                continue;   // do nothing
+
+            dir_name = forward_stack[forward_stack.size() - 1];
+            back_stack.push_back(dir_name);
+            forward_stack.pop_back();
+            return RES_GOTO_DIR;
+
+        } else if (ch == BACKSPACE) {
+            // Behaves just like '..'
+            if (back_stack.size() == 1)
+                continue; // do nothing
+
+            forward_stack.push_back(back_stack[back_stack.size() - 1]);
+            back_stack.pop_back();
+            dir_name = "..";
+            return RES_GOTO_DIR;
+
+        } else if (ch == HOME) {
+            if (back_stack.size() == 1)
+                continue;
+
+            build_home_path(back_stack, dir_name);
+            back_stack.clear();
+            forward_stack.clear();
+            back_stack.emplace_back(".");
+            return RES_GOTO_DIR;
+
+        } else if (ch == ENTER) {
+            //todo open dir/file pointed to by curremt_index
+            dir_name = directory_list[current_index];
+
+            if (dir_name == ".")
+                // do nothing skip the loop
+                continue;
+
+            if (dir_name == "..") {
+                if (back_stack.size() == 1)
+                    continue; // do nothing
+
+                forward_stack.push_back(back_stack[back_stack.size() - 1]);
+                back_stack.pop_back();
+
+            } else {
+                forward_stack.clear();
+                back_stack.push_back(dir_name);
+            }
+            return RES_GOTO_DIR;
+
+        } else if (ch == ETX) {
+            // ETX (end of text) (Ctrl+C )
+            cout << "\nType q to quit\n";
+        } else if (ch == QUIT) {
+            return RES_QUIT;
+        } else if (ch == COLON) {
+            return RES_COMMAND_MODE;
+        } else {
+            // cout << "\n You entered: " << ch << '\n';
+        }
+    }
 }
--- normal_mode.h
+++ normal_mode.h
@@ -3,9 +3,16 @@
 
 #include <iostream>
 #include <termios.h>
+#include <utility>
+#include <stack>
 #include "cursor_control.h"
 #include "keymap.h"
+#include "util.h"
+#include "list.h"
 
 int normal_mode();
+
+int refresh_normal_mode(std::string &dir_name,
+                        std::vector<std::string> &back_stack, std::vector<std::string> &forward_stack);
 
 #endif //NORMAL_MODE_H
