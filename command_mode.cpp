6dd84ad86a276046aa3f9ae54a5e9799f5cae296
Abhinav Anand <abhinav_anand@live.com>
2018-08-30 17:56:55 +0530

    Remove unnecessary termios code from command mode

--- command_mode.cpp
+++ command_mode.cpp
@@ -1,76 +1,55 @@
-#include <strings.h>
-#include <cstring>
 #include "command_mode.h"
-#include "keymap.h"
-#include "util.h"
-#include "cursor_control.h"
+#include "commands.h"
 
 using namespace std;
 
-int command_mode() {
-    int cause_of_return = RES_QUIT;
-
+int command_mode(string HOME_PATH) {
     draw_info_line("COMMAND MODE");
     draw_command_line();
 
-
-    /*
-     * Initialize termios
-    */
-    struct termios initial_settings;
-    tcgetattr(fileno(stdin), &initial_settings);
-
-    struct termios new_settings = initial_settings;
-
-    new_settings.c_lflag &= ~ICANON;
-    new_settings.c_lflag &= ~ECHO;         // to hide typed entry
-    new_settings.c_lflag &= ~ISIG;         // to catch ctrl^c etc signal
-
-    new_settings.c_cc[VMIN] = 1;
-    new_settings.c_cc[VTIME] = 0;
-
-
-    if (tcsetattr(fileno(stdin), TCSAFLUSH, &new_settings) != 0) {
-        cerr << "Could not set attributes\n";
-        return RES_ERROR;
-    }
-
-
     string command;
+    char ch;
 
-    do {
-        char ch;
-        // can't do cin>>ch https://stackoverflow.com/questions/42818899/detecting-enter-key-in-c
+    while (true) {
+        // can't do cin>>ch READ: https://stackoverflow.com/questions/42818899/detecting-enter-key-in-c
         ch = (char) cin.get();
 
-        if ((ch == BACKSPACE || ch == DELETE) && command.length() > 0) {
-            // FIXME Also deletes the colon ":"
-            cout << "\b \b";
-            if (command.length() == 1)
-                command = "";
-            else
-                command = command.substr(0, command.length() - 1);
-        } else if (ch == ESCAPE) {
-            cause_of_return = RES_NORMAL_MODE;
-            break;
-        } else if (ch == ENTER) {
-            execute_command(command);
-            command = "";   // reset command
+        if (ch == ESCAPE) {
+            return RES_NORMAL_MODE;
+        }
+
+/*
+ *      if (ch == ARROW_UP || ch == ARROW_DOWN) {
+ *          // This if will never get executed.
+ *          // RCA:=> arrow keys consist of multiple characters
+ *          // and the first character is ESC
+ *          // view README.md for more info.
+ *          // Also we should NOT do this as
+ *          // the user will then not be able to type A and B in caps
+ *          continue;   // ignore and skip the loop
+ *      }
+ */
+
+        if (ch == BACKSPACE) {
+            if (command.length() > 0) {
+                cout << "\b \b";
+                command.pop_back();
+            }
+
+            continue;   // skip rest of the loop
+        }
+
+
+        if (ch == ENTER) {
+            if (command.length() > 0) {
+            // TODO execute_command(command, HOME_PATH);
+                command.clear();   // reset command
+                draw_command_line();
+            }
         } else {
             command += ch;
             cout << ch;
         }
 
-    } while (true);
-
-    // MUST restore the terminal to the way it was
-    tcsetattr(fileno(stdin), TCSANOW, &initial_settings);
-
-    return cause_of_return;
-}
-
-void execute_command(string &command) {
-    draw_command_line();
-    cout << "Executing command :" << command;
-    // TODO: break into tokens to be used to detect the type of command
+    }
 }
--- command_mode.h
+++ command_mode.h
@@ -1,10 +1,13 @@
 #ifndef COMMAND_MODE_H
 #define COMMAND_MODE_H
 
-#include <termios.h>
 #include <iostream>
 
+#include "keymap.h"
+#include "cursor_control.h"
+#include "util.h"
 
-int command_mode();
+int command_mode(std::string HOME_PATH);
+
 
 #endif //COMMAND_MODE_H
